50bbba43f760dd89f2823a7cd27020538ce3ca1c
Hardik Jatolia <98070776+hardikjatolia@users.noreply.github.com>
2023-11-16 20:08:30 +0530

    navbar updated
    navabar updated with css

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FaTint } from 'react-icons/fa'; // Import FaTint icon from react-icons/fa
-import './navbar.scss'; // Import your SCSS file for styling if needed
+import React, { useState } from "react";
+import "../Navbar/navbar.scss";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
   return (
-    <div className="navbar">
-      
-      <Link to="/"><h1>Water Grid Management</h1></Link>
-      <div className="nav-options">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/agents">Agents</Link>
-        <Link to="/forms">Form</Link>
-      </div>
-    </div>
+    <>
+      <nav className="main-nav">
+        {/* 1st logo part  */}
+        <div className="logo">
+          <h2>
+            <span>W</span>ater
+            <span>G</span>rid
+          </h2>
+        </div>
+
+        {/* 2nd menu part  */}
+        <div
+          className={
+            showMediaIcons ? "menu-link mobile-menu-link" : "menu-link"
+          }>
+          <ul>
+            <li>
+            <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/about">About</Link>
+            </li>
+            <li>
+            <Link to="/agents">Agents</Link>
+            </li>
+            <li>
+            <Link to="/forms">Form</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* 3rd social media links */}
+        <div className="social-media">
+          {/* hamburget menu start  */}
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <GiHamburgerMenu />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </>
   );
-}
+};
 
 export default Navbar;
